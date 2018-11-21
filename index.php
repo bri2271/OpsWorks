b1a9d8d9033eea8f4d8679fd18ab995feb08664e
bri2271 <brian@bjmacdonald.com>
2018-11-20 19:51:04 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 
     <body>
 <div align="center">
-                <h1>Hello Brian, This is app 2.o</h1>
-                <h2>Congratulations! You have successfully installed app 2.0 to OpsWorks!</h2>
+                <h1>Hello Aiden!!!</h1>
+                <h2>Welcome to my service!</h2>
                 <div align="center"><img src="https://s3.amazonaws.com/bjmacdonald.com/Aiden.jpg"></div>
                 <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
                 <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
